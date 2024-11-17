557ae1e2220a871caacf916a0440818af9eb0688
rishi-code-snippets <rishisnippets@gmail.com>
2024-11-17 18:33:26 +0530

    Added Google Analytics

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { GoogleTagManager } from '@next/third-parties/google'
 import AppHeader from "./reusable-components/AppHeader";
 import AppFooter from "./reusable-components/AppFooter";
 const inter = Inter({ subsets: ["latin"] });
@@ -12,6 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <GoogleTagManager gtmId="G-ZL280RVEJV" />
       <body className={inter.className}>
       <div className="flex min-h-screen flex-col items-center justify-between">
         <div className="app flex flex-col w-full flex-1 bg-white text-blue-800">
